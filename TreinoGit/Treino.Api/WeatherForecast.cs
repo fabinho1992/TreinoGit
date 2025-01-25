bd8fed2d03af2cbee1c37afe268f10db0b36a4f0
fabinho1992 <f.santosdev1992@gmail.com>
2025-01-25 00:45:32 -0300

    first

--- TreinoGit/Treino.Api/WeatherForecast.cs
+++ TreinoGit/Treino.Api/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace Treino.Api
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.53333333);
 
         public string? Summary { get; set; }
     }
